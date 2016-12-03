b63a9eed2391ed606f6ed51da240d9b71c701be0
jack ducham <jackducham@gmail.com>
2016-12-03 15:47:54 -0600

    Add placeholder icons for current equipped lens, made UI fit on multiple common resolutions

---
+++ Assets/Scripts/Game/UI/CurrentLens.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentLens : MonoBehaviour {
+    [SerializeField]
+    Sprite[] _lens_images;
+    private Image _lens_image;
+    private int _current_lens;
+
+
+	// Use this for initialization
+	void Start () {
+        _lens_image = GetComponent<Image>();
+	}
+
+    public void DisplayLens(int idx) {
+        _current_lens = idx;
+        _lens_image.sprite = _lens_images[idx];
+    }
+
+    public void CycleLensLeft() {
+        _current_lens--;
+        if (_current_lens < 0) {
+            DisplayLens(_lens_images.Length - 1);
+        } else {
+            DisplayLens(_current_lens);
+        }
+    }
+
+    public void CycleLensRight() {
+        _current_lens++;
+        if (_current_lens >= _lens_images.Length) {
+            DisplayLens(0);
+        } else {
+            DisplayLens(_current_lens);
+        }
+    }
+}
