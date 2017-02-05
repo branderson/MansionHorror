51cf6abb0cbb4484f741ba3bc65cb217d5c47e07
Collin Hasken <chasken2@illinois.edu>
2017-02-04 18:25:22 -0600

    Chair AI updated to be more like Binding of Isaac Spiders

--- Assets/Scripts/AI/ChairAI.cs
+++ Assets/Scripts/AI/ChairAI.cs
@@ -7,8 +7,11 @@ namespace Assets.AI
 {
     public class ChairAI : CustomMonoBehaviour
     {
-        public enum Attack { Left, Right, Wait};
+        public enum Attack { Move, Wait};
         public enum Activeness { Low, Average, High };
+
+        private const int MAX_BLOCK = 2;
+        private const int MAX_COLLISIONS = 50;
 
         [SerializeField] private float _speedMax = 4;
         [SerializeField] private float _speedMin = 1;
@@ -20,26 +23,55 @@ namespace Assets.AI
         [SerializeField] protected float _shakeDuration = 0.25f;
         [SerializeField] protected float _shakeIntensity = 1.5f;
         [SerializeField] protected Activeness _activeness = Activeness.Average;
+        [SerializeField] protected float _awareRange = 7;
+
         private GameObject _player;
         private PlayerController _playerController;
         private ChairController _controller;
         private Attack _attackStatus;
+        private Vector2 _target;
+        private int moveThreshold;
+        private int _blockRight;
+        private int _blockLeft;
+        private int _blockUp;
+        private int _blockDown;
+        private int _collisions;
+
 
         private void Awake()
         {
             _player = GameObject.FindWithTag("Player");
             _playerController = _player.GetComponent<PlayerController>();
             _controller = GetComponent<ChairController>();
+            _attackStatus = Attack.Wait;
+            _blockRight = _blockLeft = _blockUp = _blockDown = _collisions = 0;
+            if (_activeness == Activeness.Low)
+                moveThreshold = 6;
+            else if (_activeness == Activeness.Average)
+                moveThreshold = 4;
+            else
+                moveThreshold = 2;
         }
 
         private void Update()
         {
-            if (_controller.MoveTowardsCharacter(_player, _attackRange, Random.Range(_speedMin, _speedMax + 1), _attackStatus))
+            if (_attackStatus != Attack.Wait)
             {
-                Vector2 direction = (_player.transform.position - transform.position).normalized;
-                _controller.Attack(_playerController, _cooldown, _damage, direction, _shakeDuration, _shakeIntensity);
+                if (_controller.MoveTowardsPosition(_target, Random.Range(_speedMin, _speedMax + 1)) || _collisions > MAX_COLLISIONS)
+                {
+                    if (!IsInvoking("NextMove"))
+                    {
+                        _attackStatus = Attack.Wait;
+                        Invoke("NextMove", Random.Range(_switchTimeMin, _switchTimeMax));
+                    }
+                }
+                if (_controller.IsCharacterWithinAttackRange(_player,_attackRange) && _controller.CanAttack())
+                {
+                    Vector2 direction = (_player.transform.position - transform.position).normalized;
+                    _controller.Attack(_playerController, _cooldown, _damage, direction, _shakeDuration, _shakeIntensity);
+                }
             }
-            if (!IsInvoking("NextMove"))
+            else if (!IsInvoking("NextMove"))
             {
                 Invoke("NextMove", Random.Range(_switchTimeMin, _switchTimeMax));
             }
@@ -47,71 +79,107 @@ namespace Assets.AI
 
         private void NextMove()
         {
+            _collisions = 0;
             int nextMove = Random.Range(0,10);
-            if (_activeness == Activeness.Low)
+
+            if (nextMove > moveThreshold)
             {
-                switch (nextMove)
+                _attackStatus = Attack.Move;
+                if (_controller.IsCharacterWithinAttackRange(_player, _attackRange, _awareRange) && _blockLeft != MAX_BLOCK && _blockRight != MAX_BLOCK && _blockUp != MAX_BLOCK && _blockDown!= MAX_BLOCK)
                 {
-                    case 0:
-                        _attackStatus = Attack.Left;
-                        break;
-                    case 1:
-                        _attackStatus = Attack.Right;
-                        break;
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                        _attackStatus = Attack.Wait;
-                        break;
+                    _target = _player.transform.position;
+                }
+                else
+                {
+                    _target = new Vector2(Random.Range(-1f,1f), Random.Range(-1f,1f));
+                    if (_blockLeft != 0 || _blockRight != 0)
+                    {
+                        if (_blockRight != 0 && _blockLeft != 0)
+                        {
+                            _target.x = Mathf.Abs(_target.y * 0.1f);
+                            _target.x *= (_blockRight > _blockLeft) ? -1 : 1;
+                        }
+                        else if ((_blockLeft != 0 && _target.x < 0) || (_blockRight != 0 && _target.x > 0))
+                            _target.x *= -1;
+                    }
+                    if (_blockDown != 0 || _blockUp != 0)
+                    {
+                        if (_blockUp != 0 && _blockDown != 0)
+                        {
+                            _target.y = Mathf.Abs(_target.x * 0.1f);
+                            _target.y *= (_blockUp > _blockDown) ? -1 : 1;
+                        }
+                        else if ((_blockDown != 0 && _target.y < 0) || (_blockUp != 0 && _target.y > 0))
+                            _target.y *= -1;
+                    }
+                    _target.Normalize();
+                    _target *= _speedMin;
+                    _target.x += _controller.transform.position.x;
+                    _target.y += _controller.transform.position.y;
+                }
+                _blockLeft -= (_blockLeft != 0) ? 1 : 0;
+                _blockRight -= (_blockRight != 0) ? 1 : 0;
+                _blockUp -= (_blockUp != 0) ? 1 : 0;
+                _blockDown -= (_blockDown != 0) ? 1 : 0;
+            }
+            else
+                _attackStatus = Attack.Wait;           
+        }
+
+        void OnCollisionStay2D(Collision2D collisionInfo)
+        {
+            _collisions++;
+            Vector2 blockingObject, currentPosition;
+            blockingObject = collisionInfo.transform.position;
+            currentPosition = _controller.transform.position;
+            if (currentPosition.x < blockingObject.x)
+            {
+                if (currentPosition.y < blockingObject.y)
+                {
+                    if (blockingObject.x - currentPosition.x > blockingObject.y - currentPosition.y)
+                    {
+                        _blockRight = (_blockRight == 0) ? MAX_BLOCK : _blockRight;
+                    }
+                    else
+                    {
+                        _blockUp = (_blockUp == 0) ? MAX_BLOCK : _blockUp;
+                    }
+                }
+                else
+                {
+                    if (blockingObject.x - currentPosition.x > currentPosition.y - blockingObject.y)
+                    {
+                        _blockRight = (_blockRight == 0) ? MAX_BLOCK : _blockRight;
+                    }
+                    else
+                    {
+                        _blockDown = (_blockDown == 0) ? MAX_BLOCK : _blockDown;
+                    }
                 }
             }
-            else if (_activeness == Activeness.Average)
+            else
             {
-                switch (nextMove)
+                if (currentPosition.y < blockingObject.y)
                 {
-                    case 0:
-                    case 1:
-                    case 2:
-                        _attackStatus = Attack.Left;
-                        break;
-                    case 3:
-                    case 4:
-                    case 5:
-                        _attackStatus = Attack.Right;
-                        break;
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                        _attackStatus = Attack.Wait;
-                        break;
+                    if (currentPosition.x - blockingObject.x > blockingObject.y - currentPosition.y)
+                    {
+                        _blockLeft = (_blockLeft == 0) ? MAX_BLOCK : _blockLeft;
+                    }
+                    else
+                    {
+                        _blockUp = (_blockUp == 0) ? MAX_BLOCK : _blockUp;
+                    }
                 }
-            }
-            else if (_activeness == Activeness.High)
-            {
-                switch (nextMove)
+                else
                 {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                        _attackStatus = Attack.Left;
-                        break;
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                        _attackStatus = Attack.Right;
-                        break;
-                    case 9:
-                        _attackStatus = Attack.Wait;
-                        break;
+                    if (currentPosition.x - blockingObject.x > currentPosition.y - blockingObject.y)
+                    {
+                        _blockLeft = (_blockLeft == 0) ? MAX_BLOCK : _blockLeft;
+                    }
+                    else
+                    {
+                        _blockDown = (_blockDown== 0) ? MAX_BLOCK : _blockDown;
+                    }
                 }
             }
         }
--- Assets/Scripts/Game/Enemies/ChairController.cs
+++ Assets/Scripts/Game/Enemies/ChairController.cs
@@ -5,57 +5,6 @@ namespace Assets.Game.Enemies
 {
     public class ChairController : EnemyController
     {
-        public bool MoveTowardsCharacter(GameObject Player, float AttackRange, float Speed, ChairAI.Attack Type)
-        {
-            if (IsCharacterWithinAttackRange(Player, AttackRange))
-            {
-                return true;
-            }
-            Vector3 Direction = Player.transform.position - transform.position;
-            Direction.z = 0;
-            Direction.Normalize();
-            Direction *= AttackRange;
-            return MoveTowardsPosition(Player.transform.position - Direction, Speed, Type);
-        }
-
-        public bool MoveTowardsPosition(Vector2 Position, float Speed, ChairAI.Attack Type)
-        {
-            int angle = 0;
-            switch (Type)
-            {
-                case ChairAI.Attack.Left:
-                    angle = 20;
-                    break;
-                case ChairAI.Attack.Right:
-                    angle = -20;
-                    break;
-                case ChairAI.Attack.Wait:
-                    return false;
-            }
-            Vector3 NewPosition = new Vector3(Position.x, Position.y, 0f);
-            if (transform.position == NewPosition)
-            {
-                return true;
-            }
-            bool left, down, newLeft, newDown;
-            NewPosition -= transform.position;
-            Vector3 Direction = NewPosition.normalized;
-            left = Direction.x < 0 ? true : false;
-            down = Direction.y < 0 ? true : false;
-            Direction = Quaternion.Euler(0, 0, angle) * Direction;
-            Direction *= Speed * Time.deltaTime;
-            newLeft = Position.x - (transform.position.x + Direction.x) < 0 ? true : false;
-            newDown = Position.y - (transform.position.y + Direction.y) < 0 ? true : false;
-            //If we overshoot, go directly to target
-            if (((left ^ newLeft) || Direction.x == 0) && ((down ^ newDown) || Direction.y == 0) || (transform.position.x + Direction.x == Position.x && transform.position.y + Direction.y == Position.y))
-            {
-                Direction.x = Position.x - transform.position.x;
-                Direction.y = Position.y - transform.position.y;
-                transform.Translate(Direction);
-                return true;
-            }
-            transform.Translate(Direction);
-            return false;
-        }
+        
     }
 }
